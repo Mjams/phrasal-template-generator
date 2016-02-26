2ac095bfb56da867c977f94015dcfb71438e4174
Mjams <maggie.paquette@hotmail.ca>
2016-02-26 10:29:21 -0500

    finished coding

--- js/main.js
+++ js/main.js
@@ -3,13 +3,19 @@ var color prompt(A colour (orange, indigo, etc.))
 var creature prompt ('A creature (dragon, moth, etc.)')
 var adj prompt('An adjective (beautiful, super, etc.)')
 var verb prompt('A past tense verb (ran, burped, etc.)')
-
 var story prompt('Which story would you like?\na) Snacking\nb) Escaping\nc) Winning\nEnter the letter below:')
 
----
+After snacking on
 
-After snacking on {adjective} treats, the {colour} bellied {creature} {verbed} for hours.
 
-Ameilia {verbed} through the {adjective} {colour} nebula escaping the space {creature}.
+if (story == a) {
+  document.write(adj + 'Treats, the ' + color + ' bellied ' + creature + " " + verb + ' for hours.')
+}
 
-Jackson chose his {adjective}, {colour} {creature} card and {verbed} it to the table knowing he won.
+if (story == b) {
+  document.write('Ameilia ' + verb + ' through the ' + adj + ' ' + color + ' nebula escaping the space ' + creature + '.')
+}
+
+if (story == c) {
+  document.write('Jackson chose his ' + adj + ',' + color + ' ' + creature + ' card and ' + verb + ' it to the table knowing he won.')
+}
