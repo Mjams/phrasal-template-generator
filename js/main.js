8ed97f0cda906581cfd94308f6463966388347f2
Mjams <maggie.paquette@hotmail.ca>
2016-02-26 10:37:10 -0500

    finished

--- js/main.js
+++ js/main.js
@@ -1,21 +1,18 @@
-alert('Let’s make a story together!')
-var color prompt(A colour (orange, indigo, etc.))
-var creature prompt ('A creature (dragon, moth, etc.)')
-var adj prompt('An adjective (beautiful, super, etc.)')
-var verb prompt('A past tense verb (ran, burped, etc.)')
-var story prompt('Which story would you like?\na) Snacking\nb) Escaping\nc) Winning\nEnter the letter below:')
+var makeStory = alert('Let’s make a story together!');
+var color = prompt('A colour (orange, indigo, etc.)');
+var creature = prompt('A creature (dragon, moth, etc.)');
+var adj =  prompt('An adjective (beautiful, super, etc.)');
+var verb = prompt('A past tense verb (ran, burped, etc.)');
+var story = prompt('Which story would you like?\na) Snacking\nb) Escaping\nc) Winning\nEnter the letter below:');
 
-After snacking on
-
-
-if (story == a) {
-  document.write(adj + 'Treats, the ' + color + ' bellied ' + creature + " " + verb + ' for hours.')
+if (story == 'a') {
+  document.write(adj + ' After snacking on treats, the ' + color + ' bellied ' + creature + ' ' + verb + ' for hours.');
 }
 
-if (story == b) {
-  document.write('Ameilia ' + verb + ' through the ' + adj + ' ' + color + ' nebula escaping the space ' + creature + '.')
+if (story == 'b') {
+  document.write('Ameilia ' + verb + ' through the ' + adj + ' ' + color + ' nebula escaping the space ' + creature + '.');
 }
 
-if (story == c) {
-  document.write('Jackson chose his ' + adj + ',' + color + ' ' + creature + ' card and ' + verb + ' it to the table knowing he won.')
+if (story == 'c') {
+  document.write('Jackson chose his ' + adj + ',' + color + ' ' + creature + ' card and ' + verb + ' it to the table knowing he won.');
 }
