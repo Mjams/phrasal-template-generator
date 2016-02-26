d505b01bddd69407d7588e932ef030ce75031cd7
Mjams <maggie.paquette@hotmail.ca>
2016-02-26 10:18:24 -0500

    added the vars

---
+++ js/main.js
@@ -0,0 +1,15 @@
+alert('Let’s make a story together!')
+var color prompt(A colour (orange, indigo, etc.))
+var creature prompt ('A creature (dragon, moth, etc.)')
+var adj prompt('An adjective (beautiful, super, etc.)')
+var verb prompt('A past tense verb (ran, burped, etc.)')
+
+var story prompt('Which story would you like?\na) Snacking\nb) Escaping\nc) Winning\nEnter the letter below:')
+
+---
+
+After snacking on {adjective} treats, the {colour} bellied {creature} {verbed} for hours.
+
+Ameilia {verbed} through the {adjective} {colour} nebula escaping the space {creature}.
+
+Jackson chose his {adjective}, {colour} {creature} card and {verbed} it to the table knowing he won.
